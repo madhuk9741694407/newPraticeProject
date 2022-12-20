2bd377c4a20e85625ee4d1219e0d403ad8fc2298
madhu <madhu@DESKTOP-61KOCTR>
2022-12-20 22:02:34 +0530

    online

---
+++ onlinegamingmini/src/main/java/pompages/loginpage.java
@@ -0,0 +1,31 @@
+package pompages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.BeforeMethod;
+
+
+
+public class loginpage 
+{
+	@BeforeMethod
+	public void openapp()
+	{
+	     
+	}
+	
+	
+	
+
+	
+	@FindBy (id="userid")
+	private WebElement username;
+	
+	@FindBy(id="pass")
+	private 
+	
+	
+	
+	
+	
+}
